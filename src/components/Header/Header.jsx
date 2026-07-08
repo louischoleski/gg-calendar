c520627340aa55217b38b559cf191ad7b41eefa1
Choleski Louis <choleski.louis@gmail.com>
2026-07-08 06:26:35 -0400

    feat: show today's date in the calendar logo
    The logo number was hardcoded to 4 via data-current-day-of-month;
    bind it to today's date.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -124,7 +124,7 @@ const Header = ({
 							<path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
 						</svg>
 					</button>
-					<div className="logo" data-current-day-of-month={4}>
+					<div className="logo" data-current-day-of-month={todayDate.getDate()}>
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
 							viewBox="0 0 36 36"
